ebe4422b3bd34bbe2a1d8a2e32bbda2acf15f2e3
Ishaan Singal <ishaansingal@gmail.com>
2013-03-26 10:57:24 +0800

    database and php integration

--- index.php
+++ index.php
@@ -213,11 +213,11 @@
 				  <select type="text" class="input-medium" style="margin-left:50px;">
 				   <?php include'db.php';
     
-					$user_query = $db->query("SELECT name FROM Guest");
-					while($user = $db->fetch_assoc($user_query))
+					$hotelBranch_query = $db->query("SELECT name FROM Hotel Branch");
+					while($hotelBranch = $db->fetch_assoc($hotelBranch_query))
 					{
 					     echo '
-						   <option>'.stripslashes($user['name']).'</option>
+						   <option>'.stripslashes($hotelBranch['city']).'</option>
 						  ';
 					}    
 				   ?>
