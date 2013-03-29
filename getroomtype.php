ef75af4434a9f4c3eaf400613572ffedba244448
Ishaan Singal <ishaansingal@gmail.com>
2013-03-30 00:49:52 +0800

    escape string removed

--- getroomtype.php
+++ getroomtype.php
@@ -1,6 +1,6 @@
 <?php include'db.php';
 
-    $choice = mysql_real_escape_string($_GET['choice']);    
+    $choice = $_GET['choice'];    
     $citychoice = mysql_real_escape_string($_GET['citychoice']);
     $hotel_query = $db->query("SELECT type FROM `Room` where hotel_name = '$choice' and hotel_city = '$citychoice'");
     while($hotel = $db->fetch_assoc($hotel_query))
