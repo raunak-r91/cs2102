390106e7ce754845c29e83311214989747a36525
Ishaan Singal <ishaansingal@gmail.com>
2013-03-29 23:29:49 +0800

    ggmu

--- index.php
+++ index.php
@@ -335,11 +335,12 @@
 	    <script>
 	       $("#city").change(function() {
 		  $("#hotel_name").load("gethotelname.php?choice=" + $("#city").val());
-		  $("#room_type").load("getroomtype.php?choice=Fortuna%20Gate&citychoice=" + $("#city").val() );
+		 		     $("#room_type").load("getroomtype.php?choice=Fortuna%20Gate&citychoice=" + $("#city").val() );		    		    
+
 
 	       });
 	       $("#hotel_name").change(function() {
-		    		    		    
+		     $("#room_type").load("getroomtype.php?choice=Fortuna%20Gate&citychoice=" + $("#city").val() );		    		    
 
 	   //    $("#room_type").load("getroomtype.php?choice=" + $("#hotel_name").val() + "&citychoice=" + $("#city").val() );		    
 	       });
