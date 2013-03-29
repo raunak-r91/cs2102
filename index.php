62838f24d4489ba1e01ead961c3e81d8d2f56615
Ishaan Singal <ishaansingal@gmail.com>
2013-03-30 01:00:51 +0800

    hotel click

--- index.php
+++ index.php
@@ -339,7 +339,7 @@
 	       });
 	       
 	       	       
-	       $($("#hotel_name").val().replace(new RegExp(" ","g"), "%20")).change(function() {
+	       $("#hotel_name").click(function() {
 	       //$("#room_type").load("getroomtype.php?choice=Fortuna%20Gate&citychoice=" + $("#city").val() );		    		    
 		    var value = $("#hotel_name").val();
 		    value = value.replace(new RegExp(" ","g"), "%20"); 
