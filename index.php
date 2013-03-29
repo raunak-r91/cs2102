dd21a98edd47455aaac9f51753f728c64f7cd0f7
Ishaan Singal <ishaansingal@gmail.com>
2013-03-29 22:18:46 +0800

    t

--- index.php
+++ index.php
@@ -234,9 +234,9 @@
 				  <select type="text" id="hotel_name" class="input-medium" style="margin-left:50px;">
 				   <?php include'db.php';
     
-					$hotel_query = $db->query("SELECT distinct city FROM `Hotel`");
-					$hotel = $db->fetch_assoc($hotel_query);
-					$hotelcity = $hotel['city'];
+					//$hotel_query = $db->query("SELECT distinct city FROM `Hotel`");
+					//$hotel = $db->fetch_assoc($hotel_query);
+					//$hotelcity = $hotel['city'];
 					$hotelname_query = $db->query("SELECT name FROM `Hotel` where city = 'Delhi'");
 					while($hotelname = $db->fetch_assoc($hotelname_query))
 					{
