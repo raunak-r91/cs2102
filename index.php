cc63801c97719e7726c6f49154757b206d718ed0
Ishaan Singal <ishaansingal@gmail.com>
2013-03-30 00:39:50 +0800

    rvp

--- index.php
+++ index.php
@@ -340,7 +340,7 @@
 	       $("#room_type").click(function() {
 	       //$("#room_type").load("getroomtype.php?choice=Fortuna%20Gate&citychoice=" + $("#city").val() );		    		    
 		    var value = $("#hotel_name").val(); // value = 9.61 use $("#text").text() if you are not on select box...
-		    value = value.replace(" ", "&#32"); // value = 9:61
+		    value = value.replace(" ", "%20"); // value = 9:61
 
 		     $("#room_type").load("getroomtype.php?choice=" + value + "&citychoice=" + $("#city").val());		    		    
 	       });
