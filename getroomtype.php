0c967928a4409af3d46ba5d499f28ab93f0c8678
Ishaan Singal <ishaansingal@gmail.com>
2013-03-29 22:59:46 +0800

    t

--- getroomtype.php
+++ getroomtype.php
@@ -2,6 +2,8 @@
     
     $choice = mysql_real_escape_string($_GET['choice']);
     $citychoice = mysql_real_escape_string($_GET['citychoice']);
+    echo '$choice';
+    echo '$citychoice';
     $hotel_query = $db->query("SELECT distinct type FROM `Room` where hotel_name = '$choice' and hotel_city = '$citychoice'");
     while($hotel = $db->fetch_assoc($hotel_query))
     {
