916938ecf4e89535ea62d2d482441c17a9384230
Ishaan Singal <ishaansingal@gmail.com>
2013-03-30 00:49:09 +0800

    debug

--- getroomtype.php
+++ getroomtype.php
@@ -1,6 +1,6 @@
 <?php include'db.php';
 
-    $choice = mmysql_real_escape_string($_GET['choice']);    
+    $choice = mysql_real_escape_string($_GET['choice']);    
     $citychoice = mysql_real_escape_string($_GET['citychoice']);
     $hotel_query = $db->query("SELECT type FROM `Room` where hotel_name = '$choice' and hotel_city = '$citychoice'");
     while($hotel = $db->fetch_assoc($hotel_query))
