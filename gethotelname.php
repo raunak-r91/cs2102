f68e2e2c7e0801cf59464ffbf08f5dc195f892d7
Ishaan Singal <ishaansingal@gmail.com>
2013-03-29 22:05:39 +0800

    debug query

--- gethotelname.php
+++ gethotelname.php
@@ -2,7 +2,7 @@
     
     $choice = mysql_real_escape_string($_GET['choice']);
 
-    $hotel_query = $db->query("SELECT name FROM `Hotel` where city =" + $choice);
+    $hotel_query = $db->query("SELECT name FROM `Hotel` where city = '$choice'");
     while($hotel = $db->fetch_assoc($hotel_query))
     {
      echo '<option>'
