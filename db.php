ee8932d57e6a2a5bd648d90e742a5423860b9cc2
Ishaan Singal <ishaansingal@gmail.com>
2013-03-29 22:30:20 +0800

    tea

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php ob_start();
-	session_start();
+	//session_start();
 
 	include $_SERVER['DOCUMENT_ROOT'].'/db.class.php';
 	$db = new database('CS2102', 'localhost', 'root', 'root'); // new instance of database class
