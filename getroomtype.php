12ffd0bb794781936c7d587e4a0972407bc94f92
Ishaan Singal <ishaansingal@gmail.com>
2013-03-30 00:18:50 +0800

    query redefined

--- getroomtype.php
+++ getroomtype.php
@@ -2,12 +2,11 @@
     
     $choice = mysql_real_escape_string($_GET['choice']);
     $citychoice = mysql_real_escape_string($_GET['citychoice']);
-     echo '<option>'.$choice.
-     '</option>';
     $hotel_query = $db->query("SELECT type FROM `Room` where hotel_name = '$choice' and hotel_city = '$citychoice'");
     while($hotel = $db->fetch_assoc($hotel_query))
     {
-     echo '<option>'.'coool'.
+     echo '<option>'
+     .stripslashes($hotel['type']).
      '</option>';
     }    
 ?>
