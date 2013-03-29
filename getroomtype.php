fd25efb9db4a6185d9067a2ff3a97d25a0857cc9
Ishaan Singal <ishaansingal@gmail.com>
2013-03-29 23:45:31 +0800

    you

--- getroomtype.php
+++ getroomtype.php
@@ -3,7 +3,7 @@
     $choice = mysql_real_escape_string($_GET['choice']);
    // $citychoice = 'Mumbai';
    // $citychoice = mysql_real_escape_string($_GET['citychoice']);
-    echo '$choice';
+    echo 'Yoo';
     $hotel_query = $db->query("SELECT type FROM `Room` where hotel_name = '$choice' and hotel_city = 'Mumbai');//'$citychoice'");
     while($hotel = $db->fetch_assoc($hotel_query))
     {
