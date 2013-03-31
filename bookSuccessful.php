0a72c4465224ef0c3aac0e51d29b0140c4ca6f71
Ishaan Singal <ishaansingal@gmail.com>
2013-04-01 02:50:52 +0800

    test booking 2.0

--- bookSuccessful.php
+++ bookSuccessful.php
@@ -8,6 +8,6 @@ echo date_format($arriveDate, 'Y-m-d');
 $departDate = date_create_from_format('j/m/Y', $_GET['departDate']);
 echo date_format($departDate, 'Y-m-d');
 
-	$db->query("INSERT into `Booking` values ('ishaans', '10101', '$hotelname', 'India', '$city', '$number', 'date_format($arriveDate, 'Y-m-d')', 'date_format($departDate, 'Y-m-d')')");  
+	$db->query("INSERT into `Booking` values ('ishaans', '10101', '$hotelname', 'India', '$city', '101', 'date_format($arriveDate, 'Y-m-d')', 'date_format($departDate, 'Y-m-d')')");  
 
 ?>
