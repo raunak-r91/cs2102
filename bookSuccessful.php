4ed625797b49500291aac980eea8817240f3b902
Ishaan Singal <ishaansingal@gmail.com>
2013-04-01 02:56:09 +0800

    testing hardcoded booking 2.1

--- bookSuccessful.php
+++ bookSuccessful.php
@@ -8,6 +8,6 @@ echo date_format($arriveDate, 'Y-m-d');
 $departDate = date_create_from_format('j/m/Y', $_GET['departDate']);
 echo date_format($departDate, 'Y-m-d');
 
-	$db->query("INSERT into `Booking` values ('ishaans', '10101', 'Fortuna Gate', 'India', 'Mumbai', 101, '2013-04-3', '2013-04-6', )");  
+	$db->query("INSERT into `Booking` values ('ishaans', '10101', 'Fortuna Gate', 'India', 'Mumbai', 101, '2013-04-3', '2013-04-6' )");  
 
 ?>
