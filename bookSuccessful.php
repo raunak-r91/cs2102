56dd128fb35073bfad7c3cfdf7ab5502c2f25e53
Ishaan Singal <ishaansingal@gmail.com>
2013-04-01 03:21:40 +0800

    final working change for insert booking

--- bookSuccessful.php
+++ bookSuccessful.php
@@ -10,6 +10,6 @@ $departDate = DateTime::createFromFormat('j/m/Y', $_GET['departDate']);
 $departDate = $departDate->format('Y-m-d');
 
 	$db->query("INSERT into `Booking` (`guest_id`, `booking_id`, `hotel_name`, `hotel_country`, `hotel_city`, `room_number`, `arrival`, `departure`)
-                   values ('ishaans', '10100', '$hotelname', 'India', '$city', 101, '$arriveDate', '$departDate'");  
+                   values ('ishaans', '10100', '$hotelname', 'India', '$city', 101, '$arriveDate', '$departDate')");  
 
 ?>
