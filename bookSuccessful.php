c70414fec4e3e7a4a5cebb67f68768606717de19
Ishaan Singal <ishaansingal@gmail.com>
2013-04-01 03:19:12 +0800

    testing auto dates

--- bookSuccessful.php
+++ bookSuccessful.php
@@ -4,12 +4,12 @@ $hotelname = $_GET['hotel_name'];
 $number = $_GET['roomNum'];
 $arriveDate = DateTime::createFromFormat('j/m/Y', $_GET['arriveDate']);
 echo $hotelname;
-echo $arriveDate->format('Y-m-d');
+$arriveDate = $arriveDate->format('Y-m-d');
 
 $departDate = DateTime::createFromFormat('j/m/Y', $_GET['departDate']);
-echo $departDate->format('Y-m-d');
+$departDate = $departDate->format('Y-m-d');
 
 	$db->query("INSERT into `Booking` (`guest_id`, `booking_id`, `hotel_name`, `hotel_country`, `hotel_city`, `room_number`, `arrival`, `departure`)
-                   values ('ishaans', '10100', '$hotelname', 'India', '$city', 101, $arriveDate->format('Y-m-d'), $departDate->format('Y-m-d'))");  
+                   values ('ishaans', '10100', '$hotelname', 'India', '$city', 101, '$arriveDate', '$departDate'");  
 
 ?>
