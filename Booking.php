184b0ef308280391601eb74ae44cef5090f6a82f
Ishaan Singal <ishaansingal@gmail.com>
2013-04-02 13:00:04 +0800

    testing booking submit 2.0

--- Booking.php
+++ Booking.php
@@ -123,10 +123,10 @@ WEBSITE : HOMEPAGE OF A HOTEL'S WEBSITE
 	  <div class="well sidebar-nav">
 	    <ul class="nav nav-list">
 			  <li class="nav-header" style="font-size:18px"><i class="icon-home"></i> HOME</li>
-			  <br/><li style="font-size:18px"><a href="Hotel Renaissance.html">Homepage</a></li>
+			  <br/><li style="font-size:18px"><a href="index.html">Homepage</a></li>
 	      <br/>
 			  <li class="nav-header" style="font-size:18px"><i class="icon-tags"></i> BOOKINGS</li>
-	      <br/><li class="active" ><a href="Booking.html" style="font-size:18px">Book Here</a></li>
+	      <br/><li class="active" ><a href="Booking.php" style="font-size:18px">Book Here</a></li>
 			  <br/><li><a href="View.html" style="font-size:18px">View Your Booking</a></li>
 			  <br/><li><a href="Modify.html" style="font-size:18px">Modify Your Booking</a></li>
 			  <br/><li><a href="Cancel.html" style="font-size:18px">Cancel Your Booking</a></li>
@@ -332,6 +332,7 @@ WEBSITE : HOMEPAGE OF A HOTEL'S WEBSITE
   if (isset($_POST['submit'])) {
     echo$_POST['city'];
   // form not submitted
+  }
   ?>
 
    
