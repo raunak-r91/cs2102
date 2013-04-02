0bc4d053bd76b3c511206650b006670d283f209f
Ishaan Singal <ishaansingal@gmail.com>
2013-04-02 18:02:32 +0800

    test booking 2.0

--- bookSuccessful.php
+++ bookSuccessful.php
@@ -4,13 +4,18 @@ session_start();
 $userid = $_SESSION['username'];
 $city = $_GET['city'];
 $hotelname = $_GET['hotel_name'];
-$number = $_GET['roomNum'];
+$number = $_GET['numRooms'];
 $arriveDate = DateTime::createFromFormat('m/j/Y', $_GET['arriveDate']);
-echo $userid;
 $arriveDate = $arriveDate->format('Y-m-d');
 
 $departDate = DateTime::createFromFormat('m/j/Y', $_GET['departDate']);
 $departDate = $departDate->format('Y-m-d');
+
+echo $userid;
+echo $hotelname;
+echo $city;
+echo $arriveDate;
+echo $departDate;
 
 $result =	$db->query("INSERT into `Booking` (`guest_id`, `hotel_name`, `hotel_country`, `hotel_city`, `room_number`, `arrival`, `departure`)
                    values ('$userid', '$hotelname', 'India', '$city', 105, '$arriveDate', '$departDate')");  
