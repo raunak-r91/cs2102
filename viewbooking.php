23ccbce95af2614d7901ab6533870b6a404481cc
Ishaan Singal <ishaansingal@gmail.com>
2013-04-05 23:37:18 +0800

    removed a button from viewbooking

--- viewbooking.php
+++ viewbooking.php
@@ -237,7 +237,6 @@ WEBSITE : HOMEPAGE OF A HOTEL'S WEBSITE
 					<br/>
 					<br/>
 					<div style="margin-left:20px">
-					<button type="submit" name="cancelbtn" class="btn btn-primary" onclick="return confirm('Are you sure you want to cancel this booking?');">Confirm Cancellation</button>
 					<a href="index.php"><button type="button" class="btn">Back</button></a>
 					</div>
         </div><!--/hererow-->
