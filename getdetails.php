168a3c665fd29162b760ba89e6f19e6c145eae1b
Raunak <raunak.r91@gmail.com>
2013-04-05 12:25:04 +0800

    testing 33

--- getdetails.php
+++ getdetails.php
@@ -3,7 +3,22 @@
     $bookingID = $_GET['id'];
     $choice = $_GET['choice'];
     
-    echo '<option>'.$bookingID .'</option>';
-    //$hotel_query = $db->query("SELECT * FROM `Booking` where booking_id = '$bookingID');
-   
+    //echo '<option>'.$bookingID .'</option>';
+    $hotel_query = $db->query("SELECT * FROM `Booking` where booking_id = '$bookingID'");
+    
+    while($hotel = $db->fetch_assoc($hotel_query))
+    {
+        if($choice == 'hotel_city') {
+            echo '<option>'
+            .stripslashes($hotel[$choice]).
+            '</option>';
+        }
+        
+        else if($choice == 'hotel_name') {
+            echo '<option>'
+            .stripslashes($hotel[$choice]).
+            '</option>';
+        }
+    }
+
 ?>
