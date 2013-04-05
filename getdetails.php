397b0ab04cdd8535bff5263fce3da15860859093
Raunak <raunak.r91@gmail.com>
2013-04-05 12:22:33 +0800

    testing 31

--- getdetails.php
+++ getdetails.php
@@ -1,9 +1,9 @@
 <?php include'db.php';
-    echo '<option> test </option>';
+    //echo '<option> test </option>';
     $bookingID = $_GET['id'];
     $choice = $_GET['choice'];
     
-    //echo '<option> $bookingID </option>';
+    echo '<option> $bookingID </option>';
     //$hotel_query = $db->query("SELECT * FROM `Booking` where booking_id = '$bookingID');
    
 ?>
