5e4b92f41daa75716b38f9a85364ada148d3ff9e
Ishaan Singal <ishaansingal@gmail.com>
2013-04-16 14:41:17 +0800

    Revert "Changed formatting"
    This reverts commit 76beb08aad192a6bb58500079b18931f732b16a9.

--- Booking.php
+++ Booking.php
@@ -360,8 +360,8 @@ WEBSITE : HOMEPAGE OF A HOTEL'S WEBSITE
 		      </div> -->
 		      
 		      <div>
-		      <strong style="margin-left:20px"> Number Of Guests </strong>
-		      <select type="text" name="numGuests" class="input-small" style="margin-left:68px;">
+		      <strong style="margin-left:28px"> Number Of Guests </strong>
+		      <select type="text" name="numGuests" class="input-small" style="margin-left:60px;">
 		      <option>1</option>
 		      <option>2</option>
 		      <option>3</option>
