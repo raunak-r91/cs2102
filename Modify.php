0989f7f5e2028e2874e6b82e2306823a5e0f8d81
Ishaan Singal <ishaansingal@gmail.com>
2013-04-16 14:41:16 +0800

    Revert "d"
    This reverts commit d098a3da123e1ae54b99a4506bae20efd3485dfc.

--- Modify.php
+++ Modify.php
@@ -186,7 +186,6 @@ session_start();
 			   OR '$departDate' BETWEEN b.`arrival` AND b.`departure`
 			   OR b.`arrival` BETWEEN '$arriveDate' AND '$departDate'
 			   OR b.`departure` BETWEEN '$arriveDate' AND '$departDate')
-			   AND b.room_number <> $currentroom
 			   
 	   )");
      
